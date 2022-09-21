3403cf67a857670b1c35edcbc4341fb7270a16fa
Yuriy Petrov <yuriy.petrov@elev.ga.ntig.se>
2022-09-21 14:53:56 +0200

    commit1

---
+++ war_simulator/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine("Welcome To War Simulator!");
+Console.WriteLine("In this war there will be two tribes in war against eachother!");
+
+Console.ReadLine();
+
+//-----------------------------------------------------------------------------------------
+Console.WriteLine("Write the name for tribeA ");
+
+string tribeA;
+tribeA = Console.ReadLine();
+Console.Beep();
+
+Console.WriteLine($"Welcome {tribeA}!");
+
+Console.ReadLine();
+//----------------------------------------------------------------------------------------
+
+Console.WriteLine("Write the name for tribeB");
+
+string tribeB;
+tribeB = Console.ReadLine();
+Console.Beep();
+
+Console.WriteLine($"Welcome {tribeB}");
+
+Console.ReadLine();
+//------------------------------------------------------------------------------------------
+
+Console.WriteLine("Both tribes will have 100Hp!");
+int tribeAhp = 100;
+int tribeBhp = 100;
+
+Console.ReadLine();
+//-----------------------------------------------------------------------------------------------
+Random number = new Random();
+
+while (tribeAhp > 0 && tribeBhp > 0)
+{
+    Console.WriteLine("New Round Starts!");
+    Console.WriteLine($"{tribeA} tribe have {tribeAhp} HP!");
+    Console.WriteLine($"{tribeB} tribe have {tribeBhp} HP!");
+    Console.Beep();
+}
+Console.ReadLine();
+//---------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
